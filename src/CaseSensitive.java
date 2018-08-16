9a2fa9898f762a282de509c219a1e12ee24a023a
Ilamparithi <il270477@L-156095641.local>
2018-08-16 20:08:06 +0100

    practising programs

---
+++ src/CaseSensitive.java
@@ -0,0 +1,20 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+
+public class CaseSensitive {
+    public static void main(String[] args) throws IOException {
+        String casesensitive = "this";
+        String caseSensitive = "that";
+        System.out.println(casesensitive + " " + caseSensitive);
+
+    /*    try (FileInputStream fs = new FileInputStream("B:/file.txt")) {
+        }
+        catch(FileNotFoundException e){
+            //do nothing
+        }
+
+        System.out.println("came after catching the exception");*/
+    }
+}
